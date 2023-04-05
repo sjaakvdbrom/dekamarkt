b952c0a09935f2be4063e8d360308199c0b6067d
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-04-05 10:35:28 +0200

    Start working on adding some data

--- app.vue
+++ app.vue
@@ -1,5 +1,41 @@
+<style scoped>
+  .container {
+    margin: 0 auto;
+    max-width: 1200px;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  .items-container {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+  .item {
+    width: 25%;
+    padding: 15px;
+    background-color: #fff;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.5)
+  }
+</style>
+
+<script setup>
+  const { data } = await useFetch('https://api.dekamarkt.nl/v1/assortmentcache/group/55/103/?api_key=6d3a42a3-6d93-4f98-838d-bcc0ab2307fd')
+</script>
+
 <template>
-  <div>
-    
+  <div class="container">
+    <div class="items-container">
+      <div v-for="product in data" :key="product.ProductID" class="item">
+        <ul>
+          <li>{{product.MainDescription}}</li>
+          <li v-if="product.SubDescription">{{product.SubDescription}}</li>
+          <li v-for="groups in product.WebSubGroups" :key="groups.WebSubGroupID">
+            {{ groups.Description }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
