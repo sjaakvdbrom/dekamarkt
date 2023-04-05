9e03ddb7bcaa04da92b44225323aec920e03ae7a
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-04-05 10:58:25 +0200

    Add test filter

--- app.vue
+++ app.vue
@@ -22,15 +22,23 @@
 
 <script setup>
   const { data } = await useFetch('https://api.dekamarkt.nl/v1/assortmentcache/group/55/103/?api_key=6d3a42a3-6d93-4f98-838d-bcc0ab2307fd')
+
+  const filter = () => {
+    data.value = data.value.filter((item) => item.MainDescription === 'Bastogne');
+  }
 </script>
 
 <template>
   <div class="container">
     <div class="items-container">
+      <div>
+        <button @click="filter">Filter bastogne</button>
+      </div>
       <div v-for="product in data" :key="product.ProductID" class="item">
         <ul>
           <li>{{product.MainDescription}}</li>
           <li v-if="product.SubDescription">{{product.SubDescription}}</li>
+          <li>{{ product.Brand }}</li>
           <li v-for="groups in product.WebSubGroups" :key="groups.WebSubGroupID">
             {{ groups.Description }}
           </li>
