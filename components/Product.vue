1022b37b1aa50c207bc43b69f043a65253c54804
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-04-11 18:50:57 +0200

    Use same name in cart as category page

--- components/Product.vue
+++ components/Product.vue
@@ -132,7 +132,7 @@
       // Else push new product to cartproducts
       cart.value.push({
         'id': productId,
-        'name': props.product.MainDescription,
+        'name': props.product.MainDescription + ' ' + props.product.SubDescription,
         'price': productPrice,
         'quantity': qty
       })
