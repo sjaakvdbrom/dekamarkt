01325b478928b4ee8d543062e9213564de823967
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2023-04-12 14:32:12 +0200

    Start working on pagination for products

--- components/Grid.vue
+++ components/Grid.vue
@@ -8,7 +8,18 @@
 </style>
 
 <script setup>
+  const originalProducts = useOriginalProducts();
   const products = useProducts();
+  const currentPage = ref(1);
+  const perPage = ref(16);
+  const onPageChange = (page) => {
+    currentPage.value = page;
+    const start = perPage.value * currentPage.value - perPage.value
+    const end = perPage.value * currentPage.value
+
+    products.value = originalProducts.value.slice(start, end)
+  }
+  products.value = originalProducts.value.slice(0, perPage.value)
 </script>
 
 <template>
@@ -16,5 +27,12 @@
     <template v-for="product in products" :key="product.ProductID">
       <Product :product="product"/>
     </template>
+    <pagination
+      v-if="originalProducts.length > perPage"
+      :totalPages="originalProducts.length / perPage"
+      :perPage="perPage"
+      :currentPage="currentPage"
+      @pagechanged="onPageChange"
+    />
   </div>
 </template>
---
+++ components/Pagination.vue
@@ -0,0 +1,82 @@
+<script setup>
+  const props = defineProps(['currentPage', 'totalPages', 'currentPage'])
+  const emit = defineEmits(['pagechanged'])
+
+  const isInFirstPage = computed(() => {
+    return props.currentPage === 1;
+  })
+
+  const isInLastPage = computed(() => {
+    return props.currentPage === props.totalPages;
+  })
+
+  const onClickFirstPage = () => {
+    emit('pagechanged', 1);
+  }
+
+  const onClickPreviousPage = () => {
+    emit('pagechanged', props.currentPage - 1);
+  }
+
+  const onClickPage = (page) => {
+    emit('pagechanged', page);
+  }
+
+  const onClickNextPage = () => {
+    emit('pagechanged', props.currentPage + 1);
+  }
+
+  const onClickLastPage = () => {
+    emit('pagechanged', props.totalPages);
+  }
+</script>
+
+<template>
+  <ul>
+    <li>
+      <button
+        type="button"
+        @click="onClickFirstPage"
+        :disabled="isInFirstPage"
+      >
+        First
+      </button>
+    </li>
+
+    <li>
+      <button
+        type="button"
+        @click="onClickPreviousPage"
+        :disabled="isInFirstPage"
+      >
+        Previous
+      </button>
+    </li>
+
+    <!-- Visible Buttons Start -->
+
+    
+
+    <!-- Visible Buttons End -->
+
+    <li>
+      <button
+        type="button"
+        @click="onClickNextPage"
+        :disabled="isInLastPage"
+      >
+        Next
+      </button>
+    </li>
+
+    <li>
+      <button
+        type="button"
+        @click="onClickLastPage"
+        :disabled="isInLastPage"
+      >
+        Last
+      </button>
+    </li>
+  </ul>
+</template>
